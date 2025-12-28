4b7df61d775d914184a83da74df42b2347da969f
Narges <nargesirezaei@gmail.com>
2025-12-28 17:46:18 +0100

    Create Database using EntityFrameWork.

---
+++ GearTalk.Web/Migrations/20251228164145_InitialMigration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GearTalk.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarReviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Heading = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PageTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FeaturedImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlHandle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublishedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Visible = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarReviews", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarCategoryCarReview",
+                columns: table => new
+                {
+                    CarCategoriesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CarReviewsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarCategoryCarReview", x => new { x.CarCategoriesId, x.CarReviewsId });
+                    table.ForeignKey(
+                        name: "FK_CarCategoryCarReview_CarCategories_CarCategoriesId",
+                        column: x => x.CarCategoriesId,
+                        principalTable: "CarCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarCategoryCarReview_CarReviews_CarReviewsId",
+                        column: x => x.CarReviewsId,
+                        principalTable: "CarReviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarCategoryCarReview_CarReviewsId",
+                table: "CarCategoryCarReview",
+                column: "CarReviewsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarCategoryCarReview");
+
+            migrationBuilder.DropTable(
+                name: "CarCategories");
+
+            migrationBuilder.DropTable(
+                name: "CarReviews");
+        }
+    }
+}
