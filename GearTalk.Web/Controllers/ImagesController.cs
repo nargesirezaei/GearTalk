886f75389662bf0460e3740b3d3f3eac14537346
Narges <nargesirezaei@gmail.com>
2026-01-04 19:07:43 +0100

    Add Froala WYSIWYG editor for Content field

---
+++ GearTalk.Web/Controllers/ImagesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GearTalk.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        //API Controller dosnt expect a view to be returned. the responses will be http responses som 404 , 200 osv
+
+
+    }
+}
